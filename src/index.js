5ed7f8c066a6c559566ff6ff1cd9f88043b1c527
WesleyReis13 <lopeswesley1392@gmail.com>
2025-09-15 15:52:54 -0300

    feat: integrar-com-fila

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+// src/index.js
 require('dotenv').config();
 const createBot = require('./bot/bot');
 const { initQueueConsumer } = require('./services/queue.consumer');
 
 async function main() {
   console.log('🤖 Iniciando serviço do WhatsApp...');
-  await createBot();
-  initQueueConsumer(sock);
-  console.log(' Aguardando escanear o QR Code...');
+  
+  try {
+    // Aguarda a criação e conexão do bot
+    const sock = await createBot(); // Inicia o Bot e retorna a instância
+    
+    // Inicia o consumidor da fila, passando a instância do Baileys
+    initQueueConsumer(sock);
+    
+    console.log('✅ Serviço inicializado. Aguardando mensagens e jobs...');
+  } catch (error) {
+    console.error('❌ Erro ao iniciar o serviço:', error);
+  }
 }
 
-main().catch(console.error);
+main();
